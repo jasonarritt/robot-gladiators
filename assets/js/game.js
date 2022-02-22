5dce51f3b1cd0732ac3ba61992937ccd6f1b2185
Jason Arritt <jsn.arritt@gmail.com>
2022-02-22 13:15:00 -0500

    added initial function for obtaining robot/player name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an Alert! Javascript is running!")
+// Function to prompt user to enter robot name
+var playerName = window.prompt("What is your robot's name?");
+
+// Create function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+console.log(playerName);
+
+//fight();
